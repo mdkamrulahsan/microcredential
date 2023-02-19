e6c9ce972bddba4c1c5fc076a01cb7b1f3ea63ed
mdkamrulahsan <121835712+mdkamrulahsan@users.noreply.github.com>
2023-02-19 18:50:24 -0500

    Add files via upload

---
+++ microcendetial/activity14/activity14.js
@@ -0,0 +1,6 @@
+console.log('Md Kamrul Ahsan Emon')
+let menu1=document.querySelctor('menu')
+let openMenu1= document.querySelector(".openMenu")
+open.Menu1.addEventListner('click,function()'
+this.clasList.toggle('active')
+)
